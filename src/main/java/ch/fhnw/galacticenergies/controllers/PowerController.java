db14aee0abb6003b03eff32626b39455360b4125
benajakuhn <benaja.kuhn@students.fhnw.ch>
2022-11-28 16:14:55 +0100

    PowerController implemented: totalPower, currentPower, calcPower

--- src/main/java/ch/fhnw/galacticenergies/controllers/PowerController.java
+++ src/main/java/ch/fhnw/galacticenergies/controllers/PowerController.java
@@ -1,16 +1,60 @@
 package ch.fhnw.galacticenergies.controllers;
 
+import ch.fhnw.galacticenergies.data.PowerInput;
+import java.util.*;
+
 public class PowerController {
 
-	private double totalPower;
+	private static double currentPower;
+	private static double totalPower;
+	private static int lastTimestamp = 0;
 
+	public static void calcPower(){
+		int currentTimestamp = 0;
+		String currentPowerStr;
+		List <List> input = new ArrayList<>();
+		while(1==1) {
+			input = PowerInput.readCSV();
+			input.remove(0);
+			currentPowerStr = (String) input.get(input.size() - 1).get(7);
+			setCurrentPower(Double.valueOf(currentPowerStr));
+
+			for (int i = 0; i < input.size(); i++) {
+				currentTimestamp = Integer.valueOf((String) input.get(i).get(2));
+				if (currentTimestamp > lastTimestamp) {
+					addTotalPower(Double.valueOf((String) input.get(i).get(7)));
+					lastTimestamp = currentTimestamp;
+				}
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ie) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+
+
+
+	public static double getCurrentPower() {
+		return currentPower;
+	}
+
+	public static void setCurrentPower(double currentPower) {
+		PowerController.currentPower = currentPower;
+	}
 
 	/**
 	 *
 	 * @return return the total value of the power.
 	 */
-	public double getTotalPower() {
-		return 0;
+	public static void addTotalPower(double power) {
+		totalPower += power;
+
+	}
+	public static double getTotalPower() {
+
+		return totalPower;
 	}
 
 	/**
--- src/main/java/ch/fhnw/galacticenergies/data/PowerInput.java
+++ src/main/java/ch/fhnw/galacticenergies/data/PowerInput.java
@@ -5,12 +5,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class PowerInput {
-    public static List readCSV() throws FileNotFoundException {
+    public static List readCSV(){
         List<String> records = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File(System.getProperty("user.dir")+"/Power.csv"));) {
             while (scanner.hasNextLine()) {
                 records.add(scanner.nextLine());
             }
+        }catch(Exception e){
+            System.err.println("Exception: " + e);
         }
         List<List> allItems = new ArrayList<>();
         for (int i = 0; i < records.size(); i++) {
