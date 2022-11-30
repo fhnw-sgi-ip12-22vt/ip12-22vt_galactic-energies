9b7e8676786524750c65f9f4e6320d2a6711e872
willi3000 <50662124+Willi3000@users.noreply.github.com>
2022-11-30 12:46:20 +0100

    Added Speed view

---
+++ src/main/java/ch/fhnw/galacticenergies/controllers/SpeedController.java
@@ -0,0 +1,13 @@
+package ch.fhnw.galacticenergies.controllers;
+
+public class SpeedController {
+    private double speed;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+}
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,40 +1,41 @@
-//import ch.fhnw.galacticenergies.controllers.PlayerController;
-//import ch.fhnw.galacticenergies.models.Player;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//public class PlayerTest {
-//
-//
-//    Player player = PlayerController.getPlayer();
-//
+import ch.fhnw.galacticenergies.controllers.PlayerController;
+import ch.fhnw.galacticenergies.models.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTest {
+
+
+    Player player = PlayerController.getPlayer();
+
 //    @Test
 //    public void testMovement() {
+//        System.out.println(player.getY_Pos());
 //        double yPos= player.getY_Pos();
 //        int counter = 0;
 //
-//        for (int i = 0; i < 10; i++) {
+//        for (int i = 1; i < 50; i=i+5) {
 //            PlayerController.moveUp();
-//            counter = i+1;
+//            counter = i;
 //        }
 //        assertEquals( yPos - counter ,player.getY_Pos());
 //        yPos= player.getY_Pos();
-//        for (int i = 0; i < 10; i++) {
-//            PlayerController.moveDown();
-//            counter = i+1;
+//        for (int i = 1; i < 50; i=i+5) {
+//            PlayerController.moveDown(500);
+//            counter = i;
 //        }
 //        assertEquals(yPos + counter,player.getY_Pos());
 //    }
-//    @Test
-//    public void testLives(){
-//        int lives = player.getLives();
-//        int counter =0;
-//
-//        for(int i = 1; i>=0; i--) {
-//            player.loseLives();
-//            counter++;
-//        }
-//        assertEquals(lives-counter,player.getLives());
-//        }
-//    }
+    @Test
+    public void testLives(){
+        int lives = player.getLives();
+        int counter =0;
+
+        for(int i = 1; i>=0; i--) {
+            player.loseLives();
+            counter++;
+        }
+        assertEquals(lives-counter,player.getLives());
+        }
+    }
