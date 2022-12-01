dd7218de27862be7b63133ba54b8be00b954e4a4
Rinor Shala <rinor.shala@students.fhnw.ch>
2022-12-01 18:07:28 +0100

    Racketen Bewegung und Pi4j

--- src/main/java/ch/fhnw/galacticenergies/GameView.java
+++ src/main/java/ch/fhnw/galacticenergies/GameView.java
@@ -3,7 +3,19 @@ package ch.fhnw.galacticenergies;
 import ch.fhnw.galacticenergies.controllers.GameViewController;
 import ch.fhnw.galacticenergies.controllers.SpeedController;
 import ch.fhnw.galacticenergies.services.WindowService;
+import com.pi4j.Pi4J;
+import com.pi4j.library.pigpio.PiGpio;
+import com.pi4j.catalog.components.Joystick;
+import com.pi4j.catalog.components.helpers.PIN;
+import com.pi4j.plugin.linuxfs.provider.i2c.LinuxFsI2CProvider;
+import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalInputProvider;
+import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalOutputProvider;
+import com.pi4j.plugin.pigpio.provider.pwm.PiGpioPwmProvider;
+import com.pi4j.plugin.pigpio.provider.serial.PiGpioSerialProvider;
+import com.pi4j.plugin.pigpio.provider.spi.PiGpioSpiProvider;
+import com.pi4j.plugin.raspberrypi.platform.RaspberryPiPlatform;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
@@ -38,6 +50,63 @@ public class GameView extends Application {
         controller.initPlayer();
         controller.showSpeed(1);
         controller.showArrow(1);
+
+        // Initialize Pi4J context
+
+        final var piGpio = PiGpio.newNativeInstance();
+        final var pi4j = Pi4J.newContextBuilder()
+                .noAutoDetect()
+                .add(new RaspberryPiPlatform() {
+                    @Override
+                    protected String[] getProviders() {
+                        return new String[]{};
+                    }
+                })
+                .add(PiGpioDigitalInputProvider.newInstance(piGpio),
+                        PiGpioDigitalOutputProvider.newInstance(piGpio),
+                        PiGpioPwmProvider.newInstance(piGpio),
+                        PiGpioSerialProvider.newInstance(piGpio),
+                        PiGpioSpiProvider.newInstance(piGpio),
+                        LinuxFsI2CProvider.newInstance()
+                )
+                .build();
+
+
+
+       // var pi4j = Pi4J.newAutoContext();
+        final var joystick = new Joystick(pi4j, PIN.D5, PIN.D6, PIN.PWM13, PIN.PWM19, PIN.D26);
+
+        joystick.whileNorth(30, () -> {
+            System.out.println("north!!!!!");
+
+            controller.moveUp();
+            controller.showArrow(3);
+        });
+        joystick.onNorth(() -> {
+            System.out.println("North !!!!!!");
+            Platform.runLater(() -> {
+                controller.moveUp();
+                controller.showArrow(3);
+            });
+
+        });
+
+        joystick.onSouth(() -> {
+                    System.out.println("South !!!!!!");
+                    Platform.runLater(() -> {
+                        controller.moveDown();
+                        controller.showArrow(2);
+                    });
+                });
+        joystick.onEast(() -> {
+            System.out.println("East !!!!!!");
+        });
+
+        joystick.onWest(() -> {
+            System.out.println("West !!!!!!");
+        });
+
+
         scene.setOnKeyPressed(e -> {
             switch (e.getCode()) {
                 case W -> {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,16 @@ module ch.fhnw.galacticenergies {
 
     requires org.controlsfx.controls;
     requires com.almasb.fxgl.all;
+    requires com.pi4j;
+    requires com.pi4j.plugin.pigpio;
+    requires com.pi4j.plugin.raspberrypi;
+    requires com.pi4j.plugin.mock;
+    requires com.pi4j.library.pigpio;
+    uses com.pi4j.extension.Extension;
+    uses com.pi4j.provider.Provider;
+    //for LCD Display
+    requires com.pi4j.plugin.linuxfs;
+    requires java.logging;
 
     exports ch.fhnw.galacticenergies;
     exports ch.fhnw.galacticenergies.controllers;
