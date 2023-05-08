dcb740e1c19dbdf5dfa2c313d692d06bbd8569b4
willi3000 <50662124+Willi3000@users.noreply.github.com>
2023-05-08 09:59:07 +0200

    Added custom menu

--- src/main/java/ch/fhnw/galacticenergies/menu/MainMenu.java
+++ src/main/java/ch/fhnw/galacticenergies/menu/MainMenu.java
@@ -2,10 +2,23 @@ package ch.fhnw.galacticenergies.menu;
 
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.scene.Node;
 
 public class MainMenu extends FXGLMenu {
-    public MainMenu(){
-        super(MenuType.MAIN_MENU);
+    public MainMenu (MenuType type) {
+        super(type);
+
+        getContentRoot().getChildren().addAll(createBackground(getAppWidth(),getAppHeight()));
     }
+
+
+
+    protected Node createBackground(double w, double h){
+        System.out.println("hallo" );
+        return FXGL.texture("bg/ColorBlend.png");
+    }
+
+
 
 }
