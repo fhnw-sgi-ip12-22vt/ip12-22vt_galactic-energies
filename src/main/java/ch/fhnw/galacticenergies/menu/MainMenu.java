cb85d8305fde639f0e0f77f9aecb19b32f7a7ef5
willi3000 <50662124+Willi3000@users.noreply.github.com>
2023-05-08 09:03:36 +0200

    Created new MainMenu Class

---
+++ src/main/java/ch/fhnw/galacticenergies/menu/MainMenu.java
@@ -0,0 +1,11 @@
+package ch.fhnw.galacticenergies.menu;
+
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.MenuType;
+
+public class MainMenu extends FXGLMenu {
+    public MainMenu(){
+        super(MenuType.MAIN_MENU);
+    }
+
+}
