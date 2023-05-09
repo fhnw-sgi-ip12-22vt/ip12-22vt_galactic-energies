4da2e1df16872c77a444374959084efdf7595ab8
willi3000 <50662124+Willi3000@users.noreply.github.com>
2023-05-09 10:21:20 +0200

    added Leaderboard

--- src/main/java/ch/fhnw/galacticenergies/menu/MainMenu.java
+++ src/main/java/ch/fhnw/galacticenergies/menu/MainMenu.java
@@ -1,39 +1,45 @@
 package ch.fhnw.galacticenergies.menu;
 
+import ch.fhnw.galacticenergies.data.DBConnection;
 import com.almasb.fxgl.animation.Animation;
 import com.almasb.fxgl.animation.Interpolators;
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
-import com.almasb.fxgl.core.Updatable;
 import com.almasb.fxgl.core.util.EmptyRunnable;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.texture.Texture;
-import javafx.animation.FadeTransition;
 import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getAssetLoader;
 
 /**
  * Creates the Main Menu
+ *
  * @Version 1.0
  */
 
 public class MainMenu extends FXGLMenu {
 
     private Animation<?> animation;
-    public MainMenu (MenuType type) {
+    private static Label leaderboard;
+
+    private static DecimalFormat df = new DecimalFormat("#.####");
+
+    public MainMenu(MenuType type) {
         super(type);
 
         Texture bg = getAssetLoader().loadTexture("bg/SpaceBackground.jpg");
@@ -41,7 +47,7 @@ public class MainMenu extends FXGLMenu {
         bg.setFitHeight(getAppHeight());
 
         Label title = new Label("Galactic Energies");
-        title.setFont(Font.font("Broadway",48));
+        title.setFont(Font.font("Broadway", 48));
         title.setTextFill(Color.WHITE);
 
         HBox titleHBox = new HBox(title);
@@ -53,8 +59,6 @@ public class MainMenu extends FXGLMenu {
         // fireNewGame() clears the Scene and calls initGame(), to spawn all entities.
         btnPlay.setOnAction(e -> fireNewGame());
 
-        Button btnLeaderboard = new Button("Leaderboard");
-        btnLeaderboard.getStyleClass().add("main_menu_button");
         Button btnExit = new Button("Exit");
         btnExit.getStyleClass().add("main_menu_button");
         btnExit.setOnAction(e -> fireExit());
@@ -63,13 +67,14 @@ public class MainMenu extends FXGLMenu {
         btnIntro.getStyleClass().add("main_menu_button");
         btnIntro.setOnAction(e -> IntroScene.start());
 
+        //Label leaderboard = createLeaderboard();
+
         VBox buttonVBox = new VBox(5, btnPlay, btnLeaderboard, btnIntro, btnExit);
         buttonVBox.getStyleClass().add("main_menu_VBox");
-        buttonVBox.setPrefWidth(getAppWidth()); buttonVBox.setAlignment(Pos.BOTTOM_LEFT); buttonVBox.setTranslateY(getAppHeight() * 0.6);
+        buttonVBox.setPrefWidth(getAppWidth());
+        buttonVBox.setAlignment(Pos.BOTTOM_LEFT);
+        buttonVBox.setTranslateY(getAppHeight() * 0.6);
         getContentRoot().getChildren().addAll(bg, titleHBox, buttonVBox);
-
-
-
 
 
         animation = FXGL.animationBuilder()
@@ -87,14 +92,53 @@ public class MainMenu extends FXGLMenu {
         animation.setOnFinished(EmptyRunnable.INSTANCE);
         animation.stop();
         animation.start();
+        // getContentRoot().getChildren().add(createLeaderboard());
     }
 
     @Override
     protected void onUpdate(double tpf) {
+        getContentRoot().getChildren().remove(leaderboard);
+        createLeaderboard();
+        getContentRoot().getChildren().add(leaderboard);
 
         animation.onUpdate(tpf);
     }
 
+    public void createLeaderboard() {
+
+        Label leaderboard = new Label();
+        leaderboard.getStyleClass().add("leaderboard");
+        leaderboard.setWrapText(true);
+        leaderboard.setLayoutX(getAppWidth() / 2);
+        leaderboard.setLayoutY(getAppHeight() * 0.25);
+
+        DBConnection c = new DBConnection();
+        Connection conn = c.getConnection();
+
+        String result = "Leaderboard: \n";
+
+        try {
+
+            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM totalpower ORDER BY producedpower DESC LIMIT 5");
+
+            while (rs.next()) {
+                Date date = rs.getDate("date");
+                Double power = rs.getDouble("producedpower");
+
+                result = result + date + "   " + df.format(power) + "  Wh \n";
+            }
+
+            conn.close();
+
+        } catch (SQLException e) {
+            System.err.println(e);
+            throw new RuntimeException(e);
+        }
+
+        leaderboard.setText(result);
+        this.leaderboard = leaderboard;
+
+    }
 
 }
 
