3886094ab21bf04d1adae0ba0a1eae886230d081
Rshala <rinor.shala@students.fhnw.ch>
2023-05-12 16:42:40 +0200

    intro datei bearbeitet

--- src/main/java/ch/fhnw/galacticenergies/menu/IntroScene.java
+++ src/main/java/ch/fhnw/galacticenergies/menu/IntroScene.java
@@ -9,10 +9,14 @@ import static com.almasb.fxgl.dsl.FXGL.getGameScene;
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getAssetLoader;
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getCutsceneService;
 
+    /**
+     * the class IntroScene sets the settings for the Introduction in the game
+    * @version 1.0
+    */
 public final class IntroScene extends GameApplication {
 
-    /*
-    *
+    /**
+    * starts the Cutscene service according to the file intro.txt
     */
     public static void start(){
         var lines = getAssetLoader().loadText("intro.txt");
@@ -20,7 +24,10 @@ public final class IntroScene extends GameApplication {
         getCutsceneService().startCutscene(cutscene, MainMenu::enableButtons);
     }
 
-
+    /**
+     *  Initializes Introduction settings
+     * @param settings sets current game settings
+     */
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setTitle("Galactic Energies");
@@ -30,6 +37,9 @@ public final class IntroScene extends GameApplication {
 
 
 
+    /**
+    * Sets the background color of the Introduction
+     */
     @Override
     protected void initGame() {
         getGameScene().setBackgroundColor(Color.LIGHTGRAY);
